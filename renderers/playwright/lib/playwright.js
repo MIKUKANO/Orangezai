9b04ea4863ff3d18d93ccafde8ed1394912c8937
MIKUKANO <mikukano@163.com>
2026-06-18 23:55:46 +0800

    细节优化

--- renderers/playwright/lib/playwright.js
+++ renderers/playwright/lib/playwright.js
@@ -2,26 +2,26 @@ import Renderer from "../../../lib/renderer/Renderer.js"
 import { BrowserManager } from "../../../lib/browser/manager.js"
 
 export default class PlaywrightRenderer extends Renderer {
-  constructor(config) {
-    super({
-      id: "playwright",
-      type: "image",
-      render: "screenshot",
-    })
-    this.manager = new BrowserManager(config)
-  }
+    constructor(config) {
+        super({
+            id: "playwright",
+            type: "image",
+            render: "screenshot"
+        })
+        this.manager = new BrowserManager(config)
+    }
 
-  async screenshot(name, data = {}) {
-    return this.manager.screenshot(name, data, {
-      dealTpl: (entryName, entryData) => this.dealTpl(entryName, entryData),
-    })
-  }
+    async screenshot(name, data = {}) {
+        return this.manager.screenshot(name, data, {
+            dealTpl: (entryName, entryData) => this.dealTpl(entryName, entryData)
+        })
+    }
 
-  restart(force = false) {
-    return this.manager.restart(force)
-  }
+    restart(force = false) {
+        return this.manager.restart(force)
+    }
 
-  async stop() {
-    await this.manager.stop()
-  }
+    async stop() {
+        await this.manager.stop()
+    }
 }
--- renderers/puppeteer/index.js
+++ renderers/puppeteer/index.js
@@ -1,7 +1,7 @@
 import PlaywrightRenderer from "../playwright/lib/playwright.js"
 
 export default function (config) {
-  const renderer = new PlaywrightRenderer(config)
-  renderer.id = "puppeteer"
-  return renderer
+    const renderer = new PlaywrightRenderer(config)
+    renderer.id = "puppeteer"
+    return renderer
 }
